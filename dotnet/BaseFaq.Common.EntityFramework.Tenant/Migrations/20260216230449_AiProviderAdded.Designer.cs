9140701513bcc26b9727b9ee32ce9860640ac794
Marcos Lima <marcosrioj@gmail.com>
2026-02-16 15:07:52 -0800

    AiProvider migration added

--- dotnet/BaseFaq.Common.EntityFramework.Tenant/Migrations/20260216230449_AiProviderAdded.Designer.cs
+++ dotnet/BaseFaq.Common.EntityFramework.Tenant/Migrations/20260216230449_AiProviderAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BaseFaq.Common.EntityFramework.Tenant;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BaseFaq.Common.EntityFramework.Tenant.Migrations
 {
     [DbContext(typeof(TenantDbContext))]
-    partial class TenantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260216230449_AiProviderAdded")]
+    partial class AiProviderAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
