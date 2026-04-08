b0a2671935d9dc92e6e8c4b9dbd4659404da4928
Marcos Lima <marcosrioj@gmail.com>
2026-04-08 11:11:16 -0700

    Add tenant user timezone migration

--- dotnet/BaseFaq.Common.EntityFramework.Tenant/Migrations/20260408180726_UserTimezoneAdded.Designer.cs
+++ dotnet/BaseFaq.Common.EntityFramework.Tenant/Migrations/20260408180726_UserTimezoneAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BaseFaq.Common.EntityFramework.Tenant;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BaseFaq.Common.EntityFramework.Tenant.Migrations
 {
     [DbContext(typeof(TenantDbContext))]
-    partial class TenantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260408180726_UserTimezoneAdded")]
+    partial class UserTimezoneAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ dotnet/BaseFaq.Common.EntityFramework.Tenant/Migrations/20260408180726_UserTimezoneAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaseFaq.Common.EntityFramework.Tenant.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTimezoneAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TimeZone",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeZone",
+                table: "Users");
+        }
+    }
+}
