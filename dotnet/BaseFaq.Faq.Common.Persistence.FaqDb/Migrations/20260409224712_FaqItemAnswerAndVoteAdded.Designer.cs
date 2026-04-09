00eb5e7bbdfa749b5130311241c9112beb9cf4d0
Marcos Lima <marcosrioj@gmail.com>
2026-04-09 15:49:05 -0700

    FaqItemAnswer and Vote migrations added

--- dotnet/BaseFaq.Faq.Common.Persistence.FaqDb/Migrations/20260409224712_FaqItemAnswerAndVoteAdded.Designer.cs
+++ dotnet/BaseFaq.Faq.Common.Persistence.FaqDb/Migrations/20260409224712_FaqItemAnswerAndVoteAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BaseFaq.Faq.Common.Persistence.FaqDb;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BaseFaq.Faq.Common.Persistence.FaqDb.Migrations
 {
     [DbContext(typeof(FaqDbContext))]
-    partial class FaqDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260409224712_FaqItemAnswerAndVoteAdded")]
+    partial class FaqItemAnswerAndVoteAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
