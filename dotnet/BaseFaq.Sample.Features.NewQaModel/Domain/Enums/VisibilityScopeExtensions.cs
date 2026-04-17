512dbf036e4a9703b4c5fff37b85212dc0e36eaa
Marcos Lima <marcosrioj@gmail.com>
2026-04-17 09:24:49 -0700

    Remove sample QnA model project

--- dotnet/BaseFaq.Sample.Features.NewQaModel/Domain/Enums/VisibilityScopeExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace BaseFaq.Sample.Features.NewQaModel.Domain.Enums;
-
-public static class VisibilityScopeExtensions
-{
-    public static bool IsPubliclyVisible(this VisibilityScope visibility) =>
-        visibility is VisibilityScope.Public or VisibilityScope.PublicIndexed;
-}
