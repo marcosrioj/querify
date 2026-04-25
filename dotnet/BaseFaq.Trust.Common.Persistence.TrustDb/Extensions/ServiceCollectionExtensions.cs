2c787d59ede5da5cf364a047751def1f79825530
Marcos Lima <marcosrioj@gmail.com>
2026-04-25 16:08:13 -0700

    Add Trust persistence context

---
+++ dotnet/BaseFaq.Trust.Common.Persistence.TrustDb/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BaseFaq.Trust.Common.Persistence.TrustDb.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddTrustDb(this IServiceCollection services)
+    {
+        services.AddDbContext<TrustDbContext>();
+
+        return services;
+    }
+}
