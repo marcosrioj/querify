2e3c7ec9c124c2cbdfeeccc708bff4ce9faa1c8c
Marcos Lima <marcosrioj@gmail.com>
2026-05-08 14:15:49 -0700

    Add source schema cleanup migration

---
+++ dotnet/Querify.QnA.Common.Persistence.QnADb/Migrations/20260508211456_SourceKindAndUploadCkecksumRemoved.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Querify.QnA.Common.Persistence.QnADb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SourceKindAndUploadCkecksumRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Kind",
+                table: "Sources");
+
+            migrationBuilder.DropColumn(
+                name: "UploadChecksum",
+                table: "Sources");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Kind",
+                table: "Sources",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UploadChecksum",
+                table: "Sources",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+    }
+}
