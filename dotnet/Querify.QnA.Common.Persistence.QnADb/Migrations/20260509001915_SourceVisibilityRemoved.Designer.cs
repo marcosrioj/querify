5c00cbf64368d55e1ebdd355ee06fab047ec34af
Marcos Lima <marcosrioj@gmail.com>
2026-05-08 17:19:48 -0700

    Add source visibility removal migration

--- dotnet/Querify.QnA.Common.Persistence.QnADb/Migrations/20260509001915_SourceVisibilityRemoved.Designer.cs
+++ dotnet/Querify.QnA.Common.Persistence.QnADb/Migrations/20260509001915_SourceVisibilityRemoved.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Querify.QnA.Common.Persistence.QnADb.DbContext;
@@ -11,9 +12,11 @@ using Querify.QnA.Common.Persistence.QnADb.DbContext;
 namespace Querify.QnA.Common.Persistence.QnADb.Migrations
 {
     [DbContext(typeof(QnADbContext))]
-    partial class QnADbContextModelSnapshot : ModelSnapshot
+    [Migration("20260509001915_SourceVisibilityRemoved")]
+    partial class SourceVisibilityRemoved
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ dotnet/Querify.QnA.Common.Persistence.QnADb/Migrations/20260509001915_SourceVisibilityRemoved.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Querify.QnA.Common.Persistence.QnADb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SourceVisibilityRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastVerifiedAtUtc",
+                table: "Sources");
+
+            migrationBuilder.DropColumn(
+                name: "Visibility",
+                table: "Sources");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastVerifiedAtUtc",
+                table: "Sources",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Visibility",
+                table: "Sources",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1);
+        }
+    }
+}
